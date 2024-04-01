141434663b483e2548479c30aa030f7f7cf79beb
Sofía Martín Pajares <sofia.martin-pajares@epitech.eu>
2024-04-01 12:49:12 +0200

    FIX: deleted printfs in function

--- src/main.c
+++ src/main.c
@@ -9,9 +9,6 @@
 
 int main(int argc, char **argv)
 {
-    for (int i = 0; argv[i] != NULL; i++)
-        printf("%s\n", argv[i]);
-    //printf("%s", argv[2]);
     if (error_handling(argc, argv) == ERROR)
         return FAIL;
     return SUCCESS;
