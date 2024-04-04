df4ab35bdb587a2482baccb8bfeaf24051f5a04b
Angel Daniel Sanchez Garcia <angel-daniel.sanchez-garcia@epitech.eu>
2024-04-04 13:26:18 +0200

    FIX: change destroy function acordingly

--- src/tools/maze.c
+++ src/tools/maze.c
@@ -9,11 +9,10 @@
 
 void destroy_maze(maze *m)
 {
-    for (size_t i = 0; i < m->room_count; i ++) {
+    for (size_t i = 0; i < m->room_count; i ++)
         destroy_room(m->room[i]);
-        free(m->tunels[i]);
-    }
     free(m->room);
+    free(m->matrix);
     free(m->tunels);
     free(m);
 }
