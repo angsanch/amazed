6f9334f342ea14f6616b7b1fd306ef218dc334b7
Angel Daniel Sanchez Garcia <angel-daniel.sanchez-garcia@epitech.eu>
2024-04-04 12:59:15 +0200

    FEAT: added new sctructs and cleanup headers

--- include/maze.h
+++ include/maze.h
@@ -5,8 +5,8 @@
 ** headerfile
 */
 
-#ifndef A_MAZE_D_
-    #define A_MAZE_D_
+#ifndef MAZE_H_
+    #define MAZE_H_
     #include <sys/stat.h>
     #include <fcntl.h>
     #include <stdlib.h>
@@ -14,10 +14,26 @@
     #include <stdio.h>
     #include <errno.h>
     #include <stddef.h>
+    #include "my.h"
 
     #define ERROR -1
     #define FAIL 84
     #define SUCCESS 0
+
+typedef struct room_information {
+    char *name;
+    size_t x;
+    size_t y;
+} room;
+
+typedef struct maze_contents {
+    size_t bots;//amount of bots
+    size_t start;//index of start room
+    size_t end;//index of end room
+    size_t room_count;//amount of rooms
+    room **room;//all room info
+    bool **tunels;//matrix of connections
+} maze;
 
 typedef struct info {
     int robots;
@@ -27,5 +43,10 @@ typedef struct info {
 } info_t;
 
 int error_handling(int argc, char **argv);
+
+//Structs
+room *create_room(char *name, size_t x, size_t y);
+void destroy_room(room *r);
+void destroy_maze(maze *m);
 
 #endif
