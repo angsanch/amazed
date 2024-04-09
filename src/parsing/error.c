b28381baaf686254e0209eb83cc436c411c25bda
Sofía Martín Pajares <sofia.martin-pajares@epitech.eu>
2024-04-09 12:24:52 +0200

    FIX: fixed coding style in error.c file

--- src/parsing/error.c
+++ src/parsing/error.c
@@ -29,15 +29,15 @@ char *get_buffer(void)
 char *clean_str(char *line)
 {
     my_strreplace(line, "##", "-1");
-    for(int i = 0; i != '\n'; i++) {
+    for (int i = 0; i != '\n'; i++) {
         if (line[i] == '#')
             line[i] = '\n';
     }
     printf(line);
-    return(line);
+    return (line);
 }
 
-static int check_map()
+static int check_map(void)
 {
     maze *amazed = my_calloc(sizeof(maze), 1);
     char *buffer = NULL;
